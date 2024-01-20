ab4d5349ef8a3db74e4ea7487365539f3367e6e5
Abiorh001 <abiolaadedayo1993@gmail.com>
2024-01-20 13:42:34 +0100

    update main

--- main.py
+++ main.py
@@ -106,6 +106,6 @@ async def generate_proposal(proposal_data: JobProposalData):
 async def root():
     return {"message": "Hello World"}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
